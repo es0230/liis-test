845445176e387fa4d55acbcd57acb3a24cc61703
es0230 <apmolodykh@gmail.com>
2023-01-16 03:09:38 +0700

    Настроен переход со страницы чекера на страницу авторизации

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,10 +1,18 @@
+import { useNavigate } from 'react-router-dom';
+import { AppRoute } from '../../const';
 import LogoutIcon from '../svg/logout-icon';
 
 const NavBar = (): JSX.Element => {
+	const navigate = useNavigate();
+
+	const handleLogoutClick = () => {
+		navigate(AppRoute.Auth);
+	}
+
 	return (
 		<nav className="checker__navbar">
 			<h1 className="checker__navbar-header">Simple Hotel Check</h1>
-			<div className="checker__navbar-logout clickable">
+			<div className="checker__navbar-logout clickable" onClick={handleLogoutClick}>
 				<p className="logout-text">Выйти</p>
 				<LogoutIcon />
 			</div>
