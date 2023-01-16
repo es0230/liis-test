3f64e34786e1e9785c5f6e3e98b792b2ac41a682
es0230 <apmolodykh@gmail.com>
2023-01-17 01:26:20 +0700

    Реализована работа хлебных крошек

--- src/components/hotels/hotels-breadcrumbs/hotels-breadcrumbs.tsx
+++ src/components/hotels/hotels-breadcrumbs/hotels-breadcrumbs.tsx
@@ -1,14 +1,21 @@
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import { useAppSelector } from '../../../hooks/hooks';
+import { selectCheckIn, selectCity } from '../../../store/app-data/selectors';
+import dayjs from 'dayjs';
+import 'dayjs/locale/ru';
 
 const HotelsBreadcrumbs = (): JSX.Element => {
+	const currentCity = useAppSelector(selectCity);
+	const checkIn = useAppSelector(selectCheckIn);
+
 	return (
 		<div className="checker__hotels-header">
 			<div className="checker__hotels-breadcrumbs">
 				<div className="breadcrumbs__common">Отели</div>
 				<ChevronRightIcon sx={{ fontSize: "xxx-large", margin: "0 -16px", color: "#A7A7A7" }} />
-				<div className="breadcrumbs__location">Москва</div>
+				<div className="breadcrumbs__location">{currentCity}</div>
 			</div>
-			<div className="checker__hotels-date">07 июля 2020</div>
+			<div className="checker__hotels-date">{dayjs(checkIn).locale('ru').format('DD MMMM YYYY')}</div>
 		</div>
 	);
 };
