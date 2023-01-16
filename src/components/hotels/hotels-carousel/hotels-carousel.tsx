4b9002aa0ee062189923727ebf5f571185248d46
es0230 <apmolodykh@gmail.com>
2023-01-17 03:44:44 +0700

    Реализована работа карусели

--- src/components/hotels/hotels-carousel/hotels-carousel.tsx
+++ src/components/hotels/hotels-carousel/hotels-carousel.tsx
@@ -1,6 +1,26 @@
+import AliceCarousel from 'react-alice-carousel';
+import 'react-alice-carousel/lib/alice-carousel.css';
+
+const handleDragStart = (e: React.DragEvent<HTMLImageElement>) => e.preventDefault();
+
+const responsive = {
+	0: {
+		items: 3,
+	}
+};
+
+const items = [
+	<img className="carousel__item" src='./img/carousel-images/carousel-1.jpg' alt="forest" onDragStart={handleDragStart} role="presentation" />,
+	<img className="carousel__item" src='./img/carousel-images/carousel-2.jpg' alt="forest" onDragStart={handleDragStart} role="presentation" />,
+	<img className="carousel__item" src='./img/carousel-images/carousel-3.jpg' alt="forest" onDragStart={handleDragStart} role="presentation" />,
+	<img className="carousel__item" src='./img/carousel-images/carousel-4.jpg' alt="forest" onDragStart={handleDragStart} role="presentation" />
+];
+
 const HotelsCarousel = (): JSX.Element => {
 	return (
-		<div className="checker__photo-slider"></div>
+		<div className="checker__photo-slider">
+			<AliceCarousel items={items} responsive={responsive} infinite mouseTracking disableDotsControls disableButtonsControls />
+		</div>
 	);
 };
 
