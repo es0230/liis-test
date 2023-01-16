ec427afdbaf6c181ad38f1f58b30c46c734ee337
es0230 <apmolodykh@gmail.com>
2023-01-17 03:51:44 +0700

    Исправлено сохранение в localStorage

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,18 @@
+import throttle from 'lodash.throttle';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import App from './components/app/App';
 import { store } from './store';
+import { saveState } from './store/browser-storage';
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
+
+store.subscribe(throttle(() => {
+	saveState(store.getState());
+}, 500));
 
 root.render(
 	<React.StrictMode>
--- src/store/middlewares/local-storage-middleware.ts
+++ src/store/middlewares/local-storage-middleware.ts
@@ -1,7 +1,6 @@
 
 import { Middleware } from '@reduxjs/toolkit';
-import { store } from '..';
-import { deleteState, saveState } from '../browser-storage';
+import { deleteState } from '../browser-storage';
 import { rootReducer } from '../root-reducer';
 
 type Reducer = ReturnType<typeof rootReducer>;
@@ -12,24 +11,7 @@ export const localStorageMiddleware: Middleware<unknown, Reducer> =
 			(action) => {
 				if (action.type === 'USER/logOut') {
 					deleteState();
-
-					return next(action);
 				}
-				saveState(store.getState());
 
 				return next(action);
 			};
-
-//const localStorageMiddleware: Middleware<unknown, Reducer> = ({ getState }: typeof store) => {
-//	return (next: Dispatch) => <A extends Action>(action: A) => {
-//		if (action.type === 'USER/logOut') {
-//			localStorage.removeItem(LOCAL_STORAGE_KEY);
-//		}
-//		const result = next(action);
-//		localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(
-//			getState()
-//		));
-//		console.log('lol');
-//		return result;
-//	};
-//};
